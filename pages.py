f6c4e559e96772de1373af35367840f16275f6d8
nathys <nathys12345@hotmail.com>
2014-10-23 16:38:32 +1100

    Update pages.py

--- pages.py
+++ pages.py
@@ -32,9 +32,8 @@ def main_page():
 			data = {"1": file, "2" : username, "3": username, "4": i, "5" : j}
 			print"""
 			<div class="thumbnail-profile" id="top%(4)s-left%(5)s">
-			<input type="image" name="pageusername" value="%(3)s" class="thumbnail-image" style="color: transparent; border: 0px; z-index: 1; width: 24em; height: 23em;">
-			<img class="thumbnail-img" src="%(1)s" alt="profile of %(2)s" style="z-index: 0;"/>
-			Username: %(3)s
+			<img class="thumbnail-img" src="%(1)s" alt="profile of %(2)s"/>
+			<input type="image" name="pageusername" value="%(3)s" class="thumbnail-image" style="color: transparent; border: 0px; z-index: 500; width: 24em; height: 23em;">
 			</div>
 			""" % data
 			counter += 1

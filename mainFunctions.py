25946e855da9578462746dd67bd76f3f11b994b6
nathys <nathys12345@hotmail.com>
2014-10-29 21:30:04 +1100

    Update mainFunctions.py

--- mainFunctions.py
+++ mainFunctions.py
@@ -18,7 +18,7 @@ def get_profile():
 	return names
 
 def search_profile(searchname):
-	c.execute("SELECT username FROM users WHERE username LIKE '\%%s\%' LIMIT 10 COLLATE NOCASE" % searchname)
+	c.execute("SELECT username FROM users WHERE username LIKE '\%%s\%' COLLATE NOCASE LIMIT 10" % searchname)
 	names = c.fetchall()
 	return names
 

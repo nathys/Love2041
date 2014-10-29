e5c19a87305020eb66f65dd1370c7cad72e66022
nathys <nathys12345@hotmail.com>
2014-10-29 21:25:05 +1100

    Update pages.py

--- pages.py
+++ pages.py
@@ -29,7 +29,7 @@ def main_page(searchFlag):
 	counter = 0
 	for i in range(1,6):
 		for j in range(1,3):
-			if counter < len(users):
+			if counter < len(user):
 				name = user[counter]
 				username = "".join(name)
 				file = "./students/%s/photo00.jpg" % name

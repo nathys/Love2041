4f747a4e617a16649d4ef705608b40031e81e2b5
nathys <nathys12345@hotmail.com>
2014-10-29 21:03:48 +1100

    Update mainFunctions.py

--- mainFunctions.py
+++ mainFunctions.py
@@ -45,7 +45,7 @@ def header(title):
 		<img src="./images/logo.png" alt="Love2041" class="img-title">
 		<input class="top-bar-button" id="button1" type="submit" value="Home Page"/>
 		<input class="top-bar-button" name="searchusername" value="Search Usernames" type="text" id="button2"/>
-		<input class="top-bar-button" id="button3" type="submit" value="search">
-		<input class="top-bar-button" id="button4" type="submit" name="login" value="0">
+		<input class="top-bar-button" id="button3" type="submit" value="search"/>
+		<input class="top-bar-button" id="button4" type="submit" name="logout" value="logout"/>
 	</div>
 	"""

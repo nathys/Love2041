dd69f12a51a126ce7992d0023d2c5e1fa7bd4525
Nathan Sher <z5017806@cse.unsw.EDU.AU>
2014-10-31 11:06:25 +1100

    changed the database writer

--- database_writer.py
+++ database_writer.py
@@ -7,7 +7,7 @@ c = conn.cursor()
 
 # Create table
 c.execute('''CREATE TABLE users
-             (username TEXT, password TEXT, gender TEXT)''')
+             (username TEXT, password TEXT, gender TEXT, year INTEGER, height DOUBLE)''')
 
 direcs = os.listdir('./students/')
 idnum  = 0
@@ -18,24 +18,39 @@ for line in direcs:
 	f = open(file,"r")
 	flag = 0
 	genderflag = 0
+	yearflag = 0
+	heightflag = 0
 	for words in f:
 		if flag == 1:
 			password = words
-			flag = 0;
+			flag = 0
 		if genderflag == 1:
 			if re.search("^\s*female",words):
 				gender = "female"
 			else:
 				gender = "male"
 			genderflag = 0
-
-		if re.search("^\s*password:",words):
-			flag = 1;
+		
+		if yearflag == 1:
+		    year = re.sub("/[0-9]{2}/[0-9]{2}", "",words)
+		    year = re.sub("\t|\n|  +", "" , year)
+		    yearflag = 0
+            
+        if heightflag == 1:
+            height = re.sub("\t|\n|  +" , "" , words)
+            heigthflag = 0
+        
+        if re.search("^\s*password:",words):
+		    flag = 1
 		if re.search("^\s*gender:",words):
 			genderflag = 1
+		if re.search("\s*birthdate:", words):
+		    yearflag = 1
+		if re.search("\s*height:", words):
+		    heightflag = 1
 
-	info = {"1": username,"2":password, "3": gender}
-	c.execute("INSERT INTO users VALUES ('%(1)s','%(2)s','%(3)s')" % info ) 
+	info = {"1": username,"2":password, "3": gender, "4" : year, "5": height}
+	c.execute("INSERT INTO users VALUES ('%(1)s','%(2)s','%(3)s','%(4)s','%(5)s')" % info ) 
 	f.close()
 	
 

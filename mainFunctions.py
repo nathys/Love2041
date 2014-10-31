d643934634ae7e116a9f45e032a34054219d4d7a
Nathan Sher <nathys123452hotmail.com>
2014-10-31 12:04:08 +1100

    minor changes

--- mainFunctions.py
+++ mainFunctions.py
@@ -33,12 +33,9 @@ def get_profile(username):
 				genderFlag = 1
 		pref.close()
 		c.execute("SELECT year FROM users WHERE username = '%s'" % username)
-		year = c.fetchone()
-		print """
-		<h1> %s </h1>
-		""" % year
+		year = int(c.fetchone())
 		info = {"1" : gender , "2": year}
-		c.execute("SELECT username FROM users WHERE gender ='%(1)s' ORDER by ABS(year - %(2)s)" % info)
+		c.execute("SELECT username FROM users WHERE gender = '%(1)s' ORDER by ABS(year - %(2)s)" % info)
 		names = c.fetchall()
 		f = open("./students/%s/matches" % username, "w+")
 		for users in names:
